f839462ed3571667532d19875cd51ff432e8dfd0
Mariya Tsybenko <mariacybenko19@gmail.com>
2026-01-14 19:45:25 +0500

    fixed mistake

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import readlineSync from 'readline-sync'
 import { greeting } from '../src/cli.js'
 
+greeting();
 const playGame = (game) => {
-  greeting();
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(game.description)
